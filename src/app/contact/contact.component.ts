6618fda3b44176577e14b075f8c385b5306dbae8
coolxjay <coolxjay@gmail.com>
2018-06-15 17:55:37 +0900

    Rxjs contactForm

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
@@ -11,9 +13,40 @@ export class ContactComponent implements OnInit {
   lat: number = 35.571767;
   lng: number = 129.187371;
 
-  constructor() { }
+	feedbackForm: FormGroup;
+  feedback: Feedback;
+  contactType = ContactType;
+  
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
 
   ngOnInit() {
   }
-
+	
+  createForm(): void {
+    this.feedbackForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      telnum: ['', [Validators.required, Validators.pattern] ],
+      email: ['', [Validators.required, Validators.email] ],
+      agree: false,
+      contacttype: 'None',
+      message: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)] ]
+    });
+  }
+  
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+  }
 }
