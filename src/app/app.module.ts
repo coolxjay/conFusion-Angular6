fa7cad4677fec13d1e5ca0ef02fe717097affef6
coolxjay <coolxjay@gmail.com>
2018-06-15 17:08:57 +0900

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,19 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule, MatProgressSpinnerModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule, MatProgressSpinnerModule, MatCheckboxModule, MatDialogModule, MatSelectModule, MatDatepickerModule, MatRadioModule, MatSliderModule, MatSlideToggleModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AgmCoreModule } from '@agm/core';
 import { ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { DishService } from './services/dish.service';
 import { LeaderService } from './services/leader.service';
 import { PromotionService } from './services/promotion.service';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -25,6 +27,7 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +38,8 @@ import { ContactComponent } from './contact/contact.component';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -55,12 +59,20 @@ import { ContactComponent } from './contact/contact.component';
     MatSortModule,
 		MatFormFieldModule,
 		MatInputModule,
+		MatCheckboxModule,
+		MatDialogModule,
+		MatProgressSpinnerModule,
+		MatSelectModule,
+		MatDatepickerModule,
+		MatRadioModule,
+		MatSliderModule,
+		MatSlideToggleModule,
 		HttpClientModule,
 		FlexLayoutModule,
 		AppRoutingModule,
-		MatProgressSpinnerModule,
 		CommonModule,
 		FormsModule,
+		ReactiveFormsModule,
 		AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDfpyLehThhRhYPQtZyb1mNmBXfwb0lZsg'
     })
@@ -70,6 +82,9 @@ import { ContactComponent } from './contact/contact.component';
 		LeaderService,
 		PromotionService
 	],
+	 entryComponents: [
+        LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
