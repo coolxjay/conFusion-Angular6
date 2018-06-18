d303064beb6b90441a72340e1ab8c9c8bc0806bc
coolxjay <coolxjay@gmail.com>
2018-06-18 18:37:22 +0900

    DataSource part 1

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { User } from '../shared/user';
 import { UserService } from '../services/user.service';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-admin',
@@ -9,7 +10,7 @@ import { UserService } from '../services/user.service';
 })
 export class AdminComponent implements OnInit {
   displayedColumns = ['id', 'username', 'firstname', 'lastname', 'admin'];
-  data: User[] = [];
+  dataSource: User[] = [];
 
   constructor(
 		private userService: UserService	
@@ -17,7 +18,7 @@ export class AdminComponent implements OnInit {
 
   ngOnInit() {
 		this.userService.getUsers()
-		.subscribe(data => this.data = data);
+		.subscribe(data => this.dataSource = data);
   }
 }
 
